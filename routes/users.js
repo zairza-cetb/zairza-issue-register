6cd35cbefe6ef9319a39517de6e6313c6d753245
Sandeep <sandeep.mahapatra.5@gmail.com>
2017-05-04 14:45:25 +0530

    error fixed

--- routes/users.js
+++ routes/users.js
@@ -1,54 +1,41 @@
 var express = require('express');
 var router = express.Router();
 var User = require('../models/user');
-var router = express.Router();
 
 module.exports = router;
 
-// function authenticateUser(req,res,next){
-//   if(req.session){
-//     if(req.session.phone && req.session.name && req.session.password){
-//       next()
-//     }
-//     else{
-//       res.redirect('/')
-//     }
-//   }
-//   else{
-//     res.redirect('/')
-//   }
-// }
-
-/* GET users listing. */
 router.post('/login',function(req,res,next) {
+  console.log('here');
   if(!req.body){
     res.send("Please send some data");
   }
-  issueRegister.findone({phone: req.body.phone}, function(err, found_user){
-    if(err){
-      res.send(err);
-    }
-    else {
-      if(found_user){
-        res.send(found_user)
+  else{
+    User.findOne({phone: req.body.phone}, function(err, foundUser){
+      if(err){
+        res.send(err);
       }
       else {
-        var newUser = new User({
-          name: req.body.name,
-          phone: req.body.phone,
-          password: req.body.password
-        });
-        newUser.save();
-        console.log(newUser);
-        res.send(newUser)
+        if(foundUser){
+          res.send(foundUser)
+        }
+        else {
+          var newUser = new User({
+            name: req.body.name,
+            phone: req.body.phone,
+            password: req.body.password
+          });
+          newUser.save(function (err, savedUser) {
+            if(err){
+              res.send(err)
+            }
+            else {
+              res.send(savedUser)
+            }
+          });
+        }
       }
-    }
-  });
+    });
+  }
 });
 
-// router.post('/users/dashboard',function(req,res,next) {
-//   res.render('dashboard')
-// })
-//
-router.get('/');
 module.exports = router;
